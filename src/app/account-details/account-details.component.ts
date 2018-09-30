6da8dc7be5a3ae451cad5090e2df15519b41137a
Lewis Law <lewis_law@msn.com>
2018-09-30 16:00:53 +1000

    account details added

--- src/app/account-details/account-details.component.ts
+++ src/app/account-details/account-details.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpHandler } from '@angular/common/http';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+import { FormsModule } from '@angular/forms';
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-account-details',
   templateUrl: './account-details.component.html',
@@ -7,9 +14,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private form: FormsModule, private httpClient: HttpClient) { }
+  private apiURL = 'http://localhost:3000/api/';
 
+  username: string;
+  userArr = [];
+  groupArr = [];
+  detailArr = [];
   ngOnInit() {
+    //determine if user logged in
+    if (!sessionStorage.getItem('username')) {
+      console.log('Not validated');
+      sessionStorage.clear();
+      alert("User not logged in");
+      this.router.navigateByUrl('login');
+    } else {
+      this.username = sessionStorage.getItem('username');
+      this.httpClient.get(this.apiURL + 'user/read')
+        .subscribe((data: any) => {
+          for (var i = 0; i < data.length; i++) {
+            if (data[i].username == this.username) {
+              this.userArr=data[i]
+              console.log(this.userArr);
+            }
+          }
+        });
+      this.httpClient.get(this.apiURL + 'group/read')
+        .subscribe((data: any) => {
+          this.groupArr = data;
+          console.log(this.groupArr);
+          for (var i = 0; i < this.groupArr.length; i++) {
+            for (var j = 0; j < this.groupArr[i].User.length; j++) {
+              if (this.groupArr[i].User[j] == this.username) {
+                this.detailArr.push('Group: ' + this.groupArr[i].GroupName );
+                for (var k = 0; k < this.groupArr[i].Channel.length; k++) {
+                  for (var x = 0; x < this.groupArr[i].Channel[k].user.length; x++) {
+                    if (this.groupArr[i].Channel[k].user[x] == this.username) {
+                      this.detailArr.push( 'Channel: ' + this.groupArr[i].Channel[k].name );
+                      
+                    }
+                  }
+                }
+              }
+            }
+          }
+          console.log(this.detailArr);
+        });
+
+    }
+
   }
 
 }
