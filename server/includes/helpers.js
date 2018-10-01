6bed99a081d33a4eb799e56016f00f20a034602e
Lewis Law <lewis_law@msn.com>
2018-10-01 17:24:34 +1000

    unit test added delete user

--- server/includes/helpers.js
+++ server/includes/helpers.js
@@ -1,4 +1,6 @@
 module.exports = {
+
+  // Register User
   register: function (uname, upwd, uemail, urole) {
     let err = [];
     if (uname == "" || uname == undefined || uname == null) {
@@ -8,10 +10,19 @@ module.exports = {
       err.push('Password is not defined')
     }
     if (uemail == "" || uemail == undefined || uemail == null) {
-      err.push('Role is not defined')
+      err.push('Email is not defined')
     }
     if (urole == "" || urole == undefined || urole == null) {
       err.push('Role is not defined')
+    }
+    return { 'errors': err };
+  },
+
+    // Delete User
+  delete: function (uname) {
+    let err = [];
+    if (uname == "" || uname == undefined || uname == null) {
+      err.push('Name is not defined')
     }
     return { 'errors': err };
   }
---
+++ server/unitTest/test.js
@@ -0,0 +1,59 @@
+var assert = require('assert');
+const express = require('express');
+const app = express();
+const helpers = require('../includes/helpers.js');
+
+describe('Always true test', function () {
+  it('should always equal true 1=1', function () {
+    assert.equal(1, 1);
+  });
+});
+
+
+//Register User
+describe('Register User Function - Missing data param 1', function () {
+  it('null, "a", "a", "a" - should have one error message', function () {
+    assert.equal(helpers.register(null, "a", "a", "a").errors.length, 1);
+  });
+  it('Error Message is - Name is not defined', function () {
+    assert.equal(helpers.register(null, "a", "a", "a").errors[0], 'Name is not defined');
+  });
+});
+
+describe('Register User Function - Missing data param 2', function () {
+  it('"a", null , "a", "a" - should have one error message', function () {
+    assert.equal(helpers.register("a", null , "a", "a").errors.length, 1);
+  });
+  it('Error Message is - Password is not defined', function () {
+    assert.equal(helpers.register("a", null, "a", "a").errors[0], 'Password is not defined');
+  });
+});
+
+describe('Register User Function - Missing data param 3', function () {
+  it('"a", "a", null, "a" - should have one error message', function () {
+    assert.equal(helpers.register("a", "a", null, "a").errors.length, 1);
+  });
+  it('Error Message is - Email is not defined', function () {
+    assert.equal(helpers.register("a", "a", null, "a").errors[0], 'Email is not defined');
+  });
+});
+
+describe('Register User Function - Missing data param 4', function () {
+  it('"a", "a", "a", null - should have one error message', function () {
+    assert.equal(helpers.register("a", "a", "a", null).errors.length, 1);
+  });
+  it('Error Message is - Role is not defined', function () {
+    assert.equal(helpers.register("a", "a", "a", null).errors[0], 'Role is not defined');
+  });
+});
+
+// Delete user
+describe('Delete User Function - Missing data param 1', function () {
+  it('null - should have one error message', function () {
+    assert.equal(helpers.delete(null).errors.length, 1);
+  });
+  it('Error Message is - Name is not defined', function () {
+    assert.equal(helpers.delete(null).errors[0], 'Name is not defined');
+  });
+});
+
